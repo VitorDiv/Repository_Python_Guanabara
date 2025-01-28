edcf4499649195f33e0f1119a68e09b04775c676
Vitinho_55 <161511884+VitorDiv@users.noreply.github.com>
2025-01-27 22:05:26 -0300

    Inicial Commit

---
+++ AulaScriptsBasicos/ScriptsDesafios.py
@@ -0,0 +1,20 @@
+## Desafio 1 -------------------------------------------
+
+nome = input ('Qual é seu nome: ')
+print ('Olá',nome,'! vitor Prazer em te conhecer !')
+
+## Desafio 2 -------------------------------------------
+
+DiaNasc = input ('Qual Dia Você Nasceu: ')
+MêsNasc = input ('Qual Mês Você Nasceu: ')
+AnoNasc = input ('Qual Ano Você Nasceu: ')
+
+print('Você Nasceu Em', DiaNasc,'/', MêsNasc, '/', AnoNasc,)
+
+
+## Desafio 3 -------------------------------------------
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um segundo número: '))
+
+print('A soma dos números', n1, '+', n2, 'é igual a', n1 + n2)
