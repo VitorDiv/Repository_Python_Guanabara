d31365e2e2676d471b4b8c3b640c74e0d3c110f6
Vitinho_55 <161511884+VitorDiv@users.noreply.github.com>
2025-02-09 14:13:24 -0300

    Commit Exercícios

---
+++ Exercicios_Python/Ex_001.py
@@ -0,0 +1,3 @@
+## Crie um programa que fale Olá Mundo na tela
+
+print('Olá, Mundo!')
---
+++ Exercicios_Python/Ex_002.py
@@ -0,0 +1,8 @@
+## Leia o nome da pessoa e de boas vindas
+
+nome = input('Qual é seu nome? ')
+
+print('Prezado seja muito bem vindo ao curso em vídeo,{}!'.format(nome))
+
+## .format(nome)	Usa {} como espaço reservado e substitui pelo valor de nome	Não adiciona espaço extra
+## Vírgula ,	Junta os argumentos separando com espaço automaticamente 
