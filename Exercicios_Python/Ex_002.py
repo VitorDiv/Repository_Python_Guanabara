47f758ad34f9a0d8fde05ec8d8111694fafcacc6
Vitinho_55 <161511884+VitorDiv@users.noreply.github.com>
2025-02-09 14:22:06 -0300

    Commit Exercicios

--- Exercicios_Python/Ex_002.py
+++ Exercicios_Python/Ex_002.py
@@ -2,7 +2,7 @@
 
 nome = input('Qual é seu nome? ')
 
-print('Prezado seja muito bem vindo ao curso em vídeo,{}!'.format(nome))
+print(f'Prezado seja muito bem vindo ao curso em vídeo {nome}')
 
 ## .format(nome)	Usa {} como espaço reservado e substitui pelo valor de nome	Não adiciona espaço extra
 ## Vírgula ,	Junta os argumentos separando com espaço automaticamente 
