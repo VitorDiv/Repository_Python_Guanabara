f484196d79f48d751b9348872f455e060285ab98
Vitor Mayer <vitormayer@weg.net>
2025-03-10 18:34:52 -0300

    correção ex4

--- Facul_ServerSide/Python_Exercícios/Ex4.py
+++ Facul_ServerSide/Python_Exercícios/Ex4.py
@@ -1,8 +1,21 @@
 ##Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 
-Letra = str(input('digite uma letra para verificar se é consoante ou vogal: ')).upper()
+Letra = input('Digite uma letra para verificar se é consoante ou vogal: ').upper()
 
-if Letra == ['a','e','i','o','u']:
-    print ('Você digitou uma vogal')
+if Letra in ['A', 'E', 'I', 'O', 'U']: 
+    print('Você digitou uma vogal')
+else:
+    print('Você digitou uma consoante')
+
+
+
+
+
+
+
+Letra = input('Digite uma letra para verificar se é consoante ou vogal: ').upper()
+
+if Letra in ['A', 'E', 'I', 'O', 'U']: 
+    print('Você digitou uma vogal')
 else:
     print('Você digitou uma consoante')
--- Facul_ServerSide/Python_Exercícios/Ex8.py
+++ Facul_ServerSide/Python_Exercícios/Ex8.py
@@ -3,7 +3,7 @@
 preco1 = float(input('Digite o preço do primeiro produto: R$ '))
 preco2 = float(input('Digite o preço do segundo produto: R$ '))
 preco3 = float(input('Digite o preço do terceiro produto: R$ '))
-
+12
 if preco1 <= preco2 and preco1 <= preco3:
     print('Você deve comprar o primeiro produto, que custa R$', preco1)
 elif preco2 <= preco1 and preco2 <= preco3:
