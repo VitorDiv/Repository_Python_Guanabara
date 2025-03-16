5912f21e255dff7c85d0fdf9fcc0aabba7fd8848
Vitinho_55 <161511884+VitorDiv@users.noreply.github.com>
2025-03-16 15:14:53 -0300

    Atualizações Curso

--- AulaScriptsBasicos/ScriptsTiposPrimitivos.py
+++ AulaScriptsBasicos/ScriptsTiposPrimitivos.py
@@ -1,1 +1,9 @@
-input('Digite um Número')
+#Tipos Primitivos Entrada e Saída de dados
+
+n1 = (input('Digite um Número: '))
+n2 = (input('Digite mais um Número: '))
+s = n1+n2
+
+print('A soma dos dos números vale', s) 
+
+print(f'A soma dos dois números vale {s}')
--- Exercicios_Python/Ex_002.py
+++ Exercicios_Python/Ex_002.py
@@ -4,5 +4,5 @@ nome = input('Qual é seu nome? ')
 
 print(f'Prezado seja muito bem vindo ao curso em vídeo {nome}')
 
-## .format(nome)	Usa {} como espaço reservado e substitui pelo valor de nome	Não adiciona espaço extra
-## Vírgula ,	Junta os argumentos separando com espaço automaticamente 
+## .f Usa {} como espaço reservado e substitui pelo valor de nome Não adiciona espaço extra
+## Vírgula ,Junta os argumentos separando com espaço automaticamente 
